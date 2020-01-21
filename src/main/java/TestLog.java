0dd882c1a24906a46e533c8feb8b81a854bb8e12
zhouyunteng <zhouyunteng@yunsom.com>
2020-01-22 00:03:44 +0800

    testng 2020.1.21

---
+++ src/main/java/TestLog.java
@@ -0,0 +1,16 @@
+import org.apache.log4j.Logger;
+
+public class TestLog {
+private Logger logger= Logger.getLogger(this.getClass());
+public void  testlog(){
+        logger.error("this is error log！");
+        logger.info("this is info log！");
+        logger.debug("this is debug log!");
+        logger.warn("this is warn log!");
+        logger.fatal("this is fatal log!");
+    }
+    public  static void main(String[] args){
+        TestLog testLog=new TestLog();
+        testLog.testlog();
+    }
+}
